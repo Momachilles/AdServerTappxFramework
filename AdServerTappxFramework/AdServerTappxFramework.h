85d3a88d5cf51702a1a94f8faab0a4f008f0441f
David Alarcon <momachilles@gmail.com>
2016-10-21 12:20:19 +0200

    Initial Commit

---
+++ AdServerTappxFramework/AdServerTappxFramework.h
@@ -0,0 +1,19 @@
+//
+//  AdServerTappxFramework.h
+//  AdServerTappxFramework
+//
+//  Created by David Alarcon on 21/10/2016.
+//  Copyright © 2016 4Crew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdServerTappxFramework.
+FOUNDATION_EXPORT double AdServerTappxFrameworkVersionNumber;
+
+//! Project version string for AdServerTappxFramework.
+FOUNDATION_EXPORT const unsigned char AdServerTappxFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdServerTappxFramework/PublicHeader.h>
+
+
