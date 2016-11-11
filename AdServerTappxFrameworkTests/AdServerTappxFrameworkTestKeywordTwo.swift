5a12f72b902e07ceba07cb45dc7ffc3a1cf39658
David Alarcon <momachilles@gmail.com>
2016-11-11 13:37:32 +0100

    More tests TODO: A better way to implement tests

---
+++ AdServerTappxFrameworkTests/AdServerTappxFrameworkTestKeywordTwo.swift
@@ -0,0 +1,172 @@
+//
+//  AdServerTappxFrameworkTestKeywordTwo.swift
+//  AdServerTappxFramework
+//
+//  Created by David Alarcon on 11/11/2016.
+//  Copyright © 2016 4Crew. All rights reserved.
+//
+
+import XCTest
+
+class AdServerTappxFrameworkTestKeywordTwo: XCTestCase {
+    
+    var settings: Settings {
+        var s = Settings()
+        s.keywords = ["2"]
+        return s
+    }
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testBannerDefault() {
+        let networkExpectation = self.expectation(description: "Test banner Default OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxBanner() {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testBanner320x50() {
+        let networkExpectation = self.expectation(description: "Test banner 320x50 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxBanner(withSize: .x320y50) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testBanner728x90() {
+        let networkExpectation = self.expectation(description: "Test banner 728x90 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxBanner(withSize: .x728y90) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testBanner300x250() {
+        let networkExpectation = self.expectation(description: "Test banner 300x250 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxBanner(withSize: .x300y250) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testInterstitialDefault() {
+        let networkExpectation = self.expectation(description: "Test interstitial Default OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxInterstitial() {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testInterstitial320x480() {
+        let networkExpectation = self.expectation(description: "Test interstitial 320x480 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxInterstitial(withSize: .x320y480) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testInterstitial480x320() {
+        let networkExpectation = self.expectation(description: "Test interstitial 480x320 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxInterstitial(withSize: .x480y320) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testInterstitial768x1024() {
+        let networkExpectation = self.expectation(description: "Test interstitial 768x1024 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxInterstitial(withSize: .x768y1024) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+    
+    func testInterstitial1024x768() {
+        let networkExpectation = self.expectation(description: "Test interstitial 1024x768 OKW 2")
+        
+        AdServerTappxFramework.sharedInstance.settings = self.settings
+        
+        DataManager.tappxInterstitial(withSize: .x1024y768) {
+            print("Result: \($0)")
+            XCTAssertNotEqual($0.value(), "", "String should be javascript")
+            networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 5) { handler in
+            
+        }
+    }
+}
